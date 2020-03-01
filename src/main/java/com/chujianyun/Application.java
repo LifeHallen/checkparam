d8329c82cbd4788141f65c54a1c22f739e03053d
MLW <shanchuangye@miningyum.com>
2020-03-01 10:28:26 +0800

    添加日志模板配置，修改WebServer设置

--- src/main/java/com/chujianyun/Application.java
+++ src/main/java/com/chujianyun/Application.java
@@ -1,21 +1,45 @@
 package com.chujianyun;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.context.WebServerApplicationContext;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.boot.web.reactive.context.ReactiveWebServerInitializedEvent;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+    //日志打印异常
+    private static final Logger appLog = LoggerFactory.getLogger(Application.class);
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
     }
 
     public static void main(String[] args) {
-
-        System.out.println("程序运行前");
         SpringApplication.run(Application.class, args);
-        System.out.println("程序运行后");
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> helloworld(){
+        return route(GET("/hello-world"),
+                request->ok().body(Mono.just("Hello, world"),String.class));
+
+    }
+    @EventListener(WebServerInitializedEvent.class)
+    public void onWebServerReady(WebServerInitializedEvent event){
+        appLog.info("当前WebServer实现类为:{}",event.getWebServer().getClass().getName());
     }
 }
